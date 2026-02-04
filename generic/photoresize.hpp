9dd6ff32f8c23d2946a783ebf07832e4d2a97d6e
Thomas Perschak <social@teclab.at>
2026-02-04 10:57:02 +0100

    swig 4.4 compatibility

--- generic/photoresize.hpp
+++ generic/photoresize.hpp
@@ -42,7 +42,7 @@
 
 %exception {
   try {
-    $function
+    $action
   } catch (const std::string &msg) {
     SWIG_exception(SWIG_RuntimeError, const_cast<char*>(msg.c_str()));
   } catch (...) {
